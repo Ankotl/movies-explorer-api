42113187f6bff35179eb92eae1897de5823685c8
Anton Kotlyakov <an.kotlyakov@yandex.ru>
2022-09-25 15:09:05 +0300

    feat/сделан бэкенд

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # movies-explorer-api
+
+https://api.akotlyakov.kino.nomorepartiesxyz.ru/ 
+IP 130.193.55.30
---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const cors = require('cors');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const router = require('./routes');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const handelError = require('./middlewares/handelError');
+const { validationLogin, validationCreateUser } = require('./middlewares/validations');
+
+const options = {
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+const app = express();
+const { PORT = 3010 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.use(cors(options));
+app.use(express.json());
+app.use(requestLogger);
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
+app.use(auth);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(handelError);
+app.listen(PORT);
